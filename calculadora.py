b728bad92149c82c60259f463fb405d5f5f274cd
Karina <kserranoa16@gmail.com>
2021-01-27 14:03:26 -0600

    v1 calculadora

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+def suma(n1, n2):
+    resultado = n1 + n2
+    return resultado
+
+def division(n1, n2):
+    resultado = n1 / n2 
+    return resultado
---
+++ test.py
@@ -0,0 +1,6 @@
+import sys
+import time
+import calculadora
+
+print(calculadora.suma(2,3))
+print(calculadora.division(4,2))
