a182f50cdcf2810045bad17948a4ca445b557313
Karina <kserranoa16@gmail.com>
2021-01-27 15:43:12 -0600

    Dato mediante metadato

--- test.py
+++ test.py
@@ -4,12 +4,28 @@ import calculadora
 
 #For dato
 datosPrueba = [4,3,2,1,0,"hola", 4.0]
+'''
 print("Metadato")
 for x in range(0,len(datosPrueba)):
     print(x)
+    print("Anterior: ", x-1)
+    print("S: ", x+1)
+
 print("Dato")
 for x in datosPrueba:
     print(x)
+    print("A:", x-1)
+    print("S:", x+1)
+'''
 print("Dato mediante metadato")
 for x in range(0, len(datosPrueba)):
     print(datosPrueba[x])
+    if x > 0:
+        print("A:", datosPrueba[x-1])
+    elif x == 0:
+        print("A no existe")
+    
+    if x == len(datosPrueba):
+        print("S:", datosPrueba[x+1])
+    elif x > len(datosPrueba):
+        print("S no existe")
