6e647fc3d2d0ff032298a2add113bd3953676e90
Karina <kserranoa16@gmail.com>
2021-01-27 14:40:35 -0600

    creacion for

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,10 @@
 def suma(n1, n2):
-    resultado = n1 + n2
-    return resultado
+    try:
+        resultado = n1 + n2
+        return resultado
+    except TypeError:
+        print("No puede sumar texto")
 
 def division(n1, n2):
-    resultado = n1 / n2 
+    resultado = n1 / n2
     return resultado
--- test.py
+++ test.py
@@ -2,5 +2,14 @@ import sys
 import time
 import calculadora
 
-print(calculadora.suma(2,3))
-print(calculadora.division(4,2))
+#For dato
+datosPrueba = [4,3,2,1,0,"hola", 4.0]
+print("Metadato")
+for x in range(0,len(datosPrueba)):
+    print(x)
+print("Dato")
+for x in datosPrueba:
+    print(x)
+print("Dato mediante metadato")
+for x in range(0, len(datosPrueba)):
+    print(datosPrueba[x])
