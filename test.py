84eb805e164f62115e90c0115960abf2c463a418
Karina <kserranoa16@gmail.com>
2021-01-27 16:47:07 -0600

    Prueba con for anidado para suma

--- test.py
+++ test.py
@@ -4,31 +4,8 @@ import calculadora
 
 #For dato
 datosPrueba = [4,3,2,1,0,"hola", 4.0]
-'''
-print("Metadato")
-for x in range(0,len(datosPrueba)):
-    print(x)
-    print("Anterior: ", x-1)
-    print("S: ", x+1)
 
-print("Dato")
-for x in datosPrueba:
-    print(x)
-    print("A:", x-1)
-    print("S:", x+1)
-'''
-print("Dato mediante metadato")
-for x in range(0, len(datosPrueba)):
-    print(datosPrueba[x])
-
-    if x > 0:
-        print("A:", datosPrueba[x-1])
-    elif x == 0:
-        print("A no existe")
-    else:
-        pass
-        
-    if x < (len(datosPrueba) - 1):
-        print("S:", datosPrueba[x+1])
-    elif x == (len(datosPrueba)):
-        print("S no eiste")
+for n1 in datosPrueba:
+    for n2 in datosPrueba:
+        resultado = calculadora.suma(n1, n2)
+        print(n1,"+", n2, "=", resultado)
